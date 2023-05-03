e7d60de6fc87cfce82c6d54270538c65e37ab28b
Jacob Boyce <jboyce313@gmail.com>
2023-05-03 12:43:57 -0500

    reconfigured header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "./styles/Header.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <header>
-      <h1>Jacob Boyce</h1>
-      <div className="nav-options">
-        <p>About me</p>
-        <p>Portfolio</p>
-        <p>Contact</p>
-        <p>Resume</p>
-      </div>
-    </header>
+    <div>
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 className="my-name">Jacob Boyce</h1>
+      <div className="nav-options">
+        <p>About me</p>
+        <p>Portfolio</p>
+        <p>Contact</p>
+        <p>Resume</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
