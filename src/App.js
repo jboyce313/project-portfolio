a9f6c3012a80ff9389ac022d4a2db590421036ff
Jacob Boyce <jboyce313@gmail.com>
2023-05-03 12:30:56 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
+import "./styles/Header.css";
 
 function App() {
-  return <header></header>;
+  return (
+    <header>
+      <h1>Jacob Boyce</h1>
+      <div className="nav-options">
+        <p>About me</p>
+        <p>Portfolio</p>
+        <p>Contact</p>
+        <p>Resume</p>
+      </div>
+    </header>
+  );
 }
 
 export default App;
