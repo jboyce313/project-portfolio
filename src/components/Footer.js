280f03d1a04dbd9989d15dfc0064ce26268c7232
Jacob Boyce <jboyce313@gmail.com>
2023-05-05 10:26:49 -0500

    updated readme

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className="footer">
-      <p className="message">Built with REACT</p>
+      <p className="message">Built with React</p>
       <div className="github">
         <p>See me on GitHub</p>
         <a href="https://github.com/jboyce313">
