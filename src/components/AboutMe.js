f3003d4a494234b980ae53c052da8d08ff4910db
Jacob Boyce <jboyce313@gmail.com>
2023-05-05 09:15:46 -0500

    updated css

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,27 +5,31 @@ function AboutMe({ display, setDisplay }) {
   return (
     <div className="display aboutMe">
       <h2 className="aboutMeHeader">About me</h2>
-      <img className="myPic" src={me} alt="Me" />
-      <p className="bio">
-        Hello! My name is Jacob and I live in the Kansas City metro area. I
-        graduated from the University of Missouri - Kansas City in 2021 with a
-        BA in Economics. I have long had an interest in programming. I've done
-        some self-teaching throughout the years but, in the summer of 2022, I
-        decided I wanted to get serious about persuing a career in software
-        engineering and enrolled in the Full-Stack Coding Bootcamp at the
-        University of Kansas.Upon my completion of this bootcamp in May of 2023,
-        I will receive my full-stack certification. For more information on my
-        coding competencies, please see the{" "}
-        <span className="resumeLink" onClick={() => setDisplay("resume")}>
-          resumé
-        </span>{" "}
-        section of this site.
-      </p>
-      <p className="lastParagraph">
-        Outside of programming, I enjoy staying active. I like sports,
-        especially soccer and climbing, and I love movies and music. I am
-        recently married and a new father to a beautiful baby girl.
-      </p>
+      <div className="profile">
+        <img className="myPic" src={me} alt="Me" />
+        <div className="bio">
+          <p>
+            Hello! My name is Jacob and I live in the Kansas City metro area. I
+            graduated from the University of Missouri - Kansas City in 2021 with
+            a BA in Economics. I have long had an interest in programming. I've
+            done some self-teaching throughout the years but, in the summer of
+            2022, I decided I wanted to get serious about persuing a career in
+            software engineering and enrolled in the Full-Stack Coding Bootcamp
+            at the University of Kansas.Upon my completion of this bootcamp in
+            May of 2023, I will receive my full-stack certification. For more
+            information on my coding competencies, please see the{" "}
+            <span className="resumeLink" onClick={() => setDisplay("resume")}>
+              resumé
+            </span>{" "}
+            section of this site.
+          </p>
+          <p className="lastParagraph">
+            Outside of programming, I enjoy staying active. I like sports,
+            especially soccer and climbing, and I love movies and music. I am
+            recently married and a new father to a beautiful baby girl.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
